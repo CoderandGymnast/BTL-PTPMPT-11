19c16c6b67d5773c882cb32c07af74a8cbd1bd29
Vo Sy Hung <hungvs@mn.cluster.com>
2022-01-06 08:16:35 +0700

    [fix]: CORS

--- __main__.py
+++ __main__.py
@@ -4,16 +4,20 @@ from model import Model
 from HDFSClient import HDFSClient 
 from env import HDFS_HOST,HDFS_PORT,HDFS_PATH_DS_CSV
 from SQLClient import SQLClient
-
 import random
 
+from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['WTF_CSRF_METHODS'] = [] 
 model= None
 hdfsClient = HDFSClient(HDFS_HOST,HDFS_PORT)
 sqlClient=SQLClient(hdfsClient)
 
 @app.route('/get_recs', methods=['GET', 'POST'])
+@cross_origin()
 def get_recs():
     user_id=request.args.get("user_id")
     num_movies=request.args.get("num_movies")
@@ -21,6 +25,7 @@ def get_recs():
     return f"{res}" 
 
 @app.route('/upload', methods=['GET', 'POST'])
+@cross_origin()
 def upload():
     f = request.files['file']
     ds_type=request.form["type"]
@@ -33,6 +38,7 @@ def upload():
     return ""
 
 @app.route('/train', methods=['GET', 'POST'])
+@cross_origin()
 def train():
     if not model:
         return "[ERROR]: model have not loaded"
@@ -40,6 +46,7 @@ def train():
     return f"{res}" 
 
 @app.route('/init', methods=['GET', 'POST'])
+@cross_origin()
 def init():
     global model
     if not model: # first load.
@@ -50,23 +57,27 @@ def init():
     return "" 
 
 @app.route('/sync_sql', methods=['GET', 'POST'])
+@cross_origin()
 def sync_sql():
     sqlClient.sync_ratings()
     sqlClient.sync_movies()
     return "" 
 
 @app.route('/clean_ds_csv', methods=['GET', 'POST'])
+@cross_origin()
 def clean_ds_csv():
     hdfsClient.clean_ds_csv() 
     return "" 
 
 @app.route('/save_image', methods=['GET', 'POST'])
+@cross_origin()
 def save_image():
     url=request.args.get("url")
     name=hdfsClient.save_image(url)
     return name 
 
 @app.route('/download_ds_csv', methods=['GET', 'POST'])
+@cross_origin()
 def download_ds_csv():
     hdfsClient.download_ds_csv()
     return ""
